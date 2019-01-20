da26c8322a08933c18246e402343ebd3d2f3156e
rgirodon <remy.girodon@gmail.com>
2019-01-20 15:18:04 -0500

    Set up dependency injection with singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Managers\NewsLetterSubscriptionManager;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(
+            'App\Manager\NewsLetterSubscriptionManager',
+            function($app) {
+                return new NewsLetterSubscriptionManager();
+            });
     }
 }
