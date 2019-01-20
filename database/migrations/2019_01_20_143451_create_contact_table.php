487115fffa9f4efa820f91239dedabb46e9afe2f
rgirodon <remy.girodon@gmail.com>
2019-01-20 09:46:02 -0500

    Setup contact migration

---
+++ database/migrations/2019_01_20_143451_create_contact_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email');
+            $table->string('phone');
+            $table->text('description');
+            $table->integer('user_id');
+            
+            $table->foreign('user_id')->references('id')->on('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact');
+    }
+}
